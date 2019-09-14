6872ab9a41584b175bdc15fa8398002442c6aa1e
LiliaRibac <ribac.lilia@gmai.com>
2019-09-13 20:03:59 -0700

    fixed the error

--- microposts/src/ui.js
+++ microposts/src/ui.js
@@ -1,30 +1,29 @@
 class UI {
-  constructor(){
+  constructor() {
     this.post = document.querySelector("#posts");
     this.titleInput = document.querySelector("#title");
     this.bodyInput = document.querySelector("#body");
     this.idInput = document.querySelector("#id");
     this.postSubmit = document.querySelector(".post-submit");
-    this.forState ="add"
+    this.forState = "add"
   }
-  showPosts(posts){
- let output ="";
- 
- posts.forEach(post => {
-   output +=`
-    <div class="card mb-3>
+  showPosts(posts) {
+    let output = "";
+
+    posts.forEach((post) => {
+      output += `
+
+    <div class="card mb-3">
       <div class="card-body">
         <h4 class="card-title">${post.title}</h4>
-        <p class="card-text"> ${post.title}</p>
-        <a href="#" class="edit card-link" data-id="${post.id}">
-        <i class="fa fa-pencil"></i>
-
+        <p class="card-text"> ${post.body}</p>
+        <a href="#" class="edit card-link" data-id="${post.id}"><i class="fa fa-pencil"></i></a>
         <a href="#" class="delete card-link" data-id="${post.id}">
-        <i class="fa fa-remove"></i>
+        <i class="fa fa-remove"></i></a>
       </div>
     </div>`
- });
-this.post.innerHTML = output;
+    });
+    this.post.innerHTML = output;
   }
 }
 
