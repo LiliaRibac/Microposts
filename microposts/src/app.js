a92e1bdba2b4e906a6cabe8b4838ad91e4dbc0d2
LiliaRibac <ribac.lilia@gmai.com>
2019-09-16 10:46:48 -0700

    add function for edit btn and cancel btn

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -18,6 +18,11 @@ document.querySelector("#posts").addEventListener("click", deletePost);
 //Listen for edit state
 document.querySelector("#posts").addEventListener("click", enableEdit);
 
+//Listen for cancel
+document.querySelector(".card-form").addEventListener("click",
+  cancelEdit)
+
+
 //Get posts
 function getPosts() {
   http.get("http://localhost:3000/posts")
@@ -29,20 +34,41 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector("#title").value;
   const body = document.querySelector("#body").value;
+  const id = document.querySelector("#id").value;
 
   const data = {
     title,
     body
   }
+  //Validate input
+  if (title === "" || body === "") {
+    ui.showAlert("Please fill in all fields", "alert alert-danger")
+  } else {
 
-  //Create Post
-  http.post("http://localhost:3000/posts", data)
-    .then(data => {
-      ui.showAlert("Post added", 'alert alert-success');
-      ui.clearFields();
-      getPosts()
-    })
-    .catch(err => console.log(err))
+    //Check for id
+    if (id === '') {
+
+      //Create Post
+      http.post("http://localhost:3000/posts", data)
+        .then(data => {
+          ui.showAlert("Post added", 'alert alert-success');
+          ui.clearFields();
+          getPosts()
+        })
+        .catch(err => console.log(err))
+    } else {
+      //Update POST
+      http.put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert("Post update", 'alert alert-success');
+          ui.changeFormState("add");
+          getPosts()
+
+        })
+        .catch(err => console.log(err))
+    }
+
+  }
 }
 
 function deletePost(e) {
@@ -61,19 +87,28 @@ function deletePost(e) {
 }
 
 //Enable able state
-function enableEdit(e){
-if(e.target.parentElement.classList.contains('edit')){
-  const id = e.target.parentElement.dataset.id;
-  const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-  const body = e.target.parentElement.previousElementSibling.textContent;
-   const data = {
-     id,
-     title,
-     body
-   }
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    const data = {
+      id,
+      title,
+      body
+    }
 
-   //Fill the form
-   ui.fillForm(data)
+    //Fill the form
+    ui.fillForm(data)
+  }
+  e.preventDefault()
 }
+
+//Cncel edit state
+function cancelEdit(e) {
+  if (e.target.classList.contains("post-cancel")) {
+    ui.changeFormState("add");
+
+  }
   e.preventDefault()
 }
--- microposts/src/ui.js
+++ microposts/src/ui.js
@@ -72,7 +72,48 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+
+    this.changeFormState("edit");
   }
+
+  //Clear ID hidden value
+  clearIdInput(){
+    this.clearIdInput.value = "";
+  }
+
+  //Change Form State
+  changeFormState(type){
+    if(type === "edit"){
+      this.postSubmit.textContent ="Update Post";
+      this.postSubmit.className = "post-submit btn btn-warning btn-block";
+
+      //Create a cancel button
+      const button = document.createElement("button");
+      button.className ="post-cancel btn btn-light btn-block";
+      button.appendChild(document.createTextNode("Cancel Edit"));
+
+      //get parent
+      const cardForm = document.querySelector('.card-form');
+      //Get element to insert before
+      const formEnd = document.querySelector(".form-end");
+      //Insert cancel button
+      cardForm.insertBefore(button, formEnd)
+    }else{
+      this.postSubmit.textContent ="Post It";
+      this.postSubmit.className = "post-submit btn btn-primary btn-block";
+      //remove cancel btn if its there
+      if(document.querySelector(".post-cancel")){
+        document.querySelector(".post-cancel").remove();
+      }
+
+      //Clear ID from hidden feild
+       this.clearIdInput();
+
+       //Clear text
+       this.clearFields();
+    }
+  }
+
 }
 
 
