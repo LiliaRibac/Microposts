ec87f24a4d8fc89d77fcb793a8a22898d8f6d01b
LiliaRibac <ribac.lilia@gmai.com>
2019-09-16 09:52:22 -0700

    add function. for post edit & update

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -12,8 +12,11 @@ document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector(".post-submit").addEventListener("click",
   submitPost);
 
-  //Listen for delete
-  document.querySelector("#posts").addEventListener("click", deletePost)
+//Listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
+//Listen for edit state
+document.querySelector("#posts").addEventListener("click", enableEdit);
 
 //Get posts
 function getPosts() {
@@ -27,36 +30,50 @@ function submitPost() {
   const title = document.querySelector("#title").value;
   const body = document.querySelector("#body").value;
 
-  const data ={
+  const data = {
     title,
     body
   }
 
   //Create Post
   http.post("http://localhost:3000/posts", data)
-  .then(data => {
-    ui.showAlert("Post added", 'alert alert-success');
-    ui.clearFields();
-    getPosts()
-  })
-  .catch(err=>console.log(err))
-}
-
-function deletePost(e){
-e.preventDefault();
-if(e.target.parentElement.classList.contains("delete")){
-  const id = e.target.parentElement.dataset.id;
-  if(confirm("Are you sure?")){
-    http.delete(`http://localhost:3000/posts/${id}`)
     .then(data => {
-      ui.showAlert('Post Removed', "alert alert-success");
-      getPosts();
+      ui.showAlert("Post added", 'alert alert-success');
+      ui.clearFields();
+      getPosts()
     })
     .catch(err => console.log(err))
+}
+
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm("Are you sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', "alert alert-success");
+          getPosts();
+        })
+        .catch(err => console.log(err))
+    }
   }
 }
 
+//Enable able state
+function enableEdit(e){
+if(e.target.parentElement.classList.contains('edit')){
+  const id = e.target.parentElement.dataset.id;
+  const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+  const body = e.target.parentElement.previousElementSibling.textContent;
+   const data = {
+     id,
+     title,
+     body
+   }
 
-
-
+   //Fill the form
+   ui.fillForm(data)
+}
+  e.preventDefault()
 }
